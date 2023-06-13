78ba0104b15e90bc8d6867d9dc3a2a1f0b5e2b18
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2023-06-14 02:45:25 +0600

    Sidebar and Navbar Added

--- client/src/components/Restaurant/Home/Home.jsx
+++ client/src/components/Restaurant/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Home.scss";
+import Navbar from "./Navbar/Navbar.jsx";
+import Sidebar from "./Sidebar/Sidebar.jsx";
 import Widget from "./Widget";
 
 const Home = () => {
   return (
     <div className="home">
-      {/* SideBar */}
+      <Sidebar/>
       <div className="homeContainer">
-        {/* Navbar */}
+        <Navbar/>
         <div className="widgets"></div>
         <Widget type="customer" />
         <Widget type="review" />
