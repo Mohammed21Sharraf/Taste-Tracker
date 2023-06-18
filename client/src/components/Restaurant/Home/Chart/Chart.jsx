58b9307c2f40ad7577c59c1561110f9257ac9cf4
KaziQuader <zunayedquader@gmail.com>
2023-06-18 14:50:59 +0600

    Updated the Review and reservation circular chart. Added Line chart

--- client/src/components/Restaurant/Home/Chart/Chart.jsx
+++ client/src/components/Restaurant/Home/Chart/Chart.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import "./Chart.scss";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Chart = () => {
-  return <div className="chart">Chart</div>;
+  const data = [
+    { name: "January", Reviews: 1200, Reservations: 500 },
+    { name: "February", Reviews: 2100, Reservations: 500 },
+    { name: "March", Reviews: 800, Reservations: 500 },
+    { name: "April", Reviews: 1600, Reservations: 500 },
+    { name: "May", Reviews: 900, Reservations: 500 },
+    { name: "June", Reviews: 1700, Reservations: 500 },
+  ];
+
+  return (
+    <div className="chart">
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <LineChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Reviews" stroke="#8884d8" />
+          <Line type="monotone" dataKey="Reservations" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Chart;
--- client/src/components/Restaurant/Home/Featured/Featured.jsx
+++ client/src/components/Restaurant/Home/Featured/Featured.jsx
@@ -1,22 +1,50 @@
 import React from "react";
 import "./Featured.scss";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 const Featured = () => {
   return (
     <div className="featured">
-      <div className="top">
-        <h1 className="title">Total Reviews</h1>
-        <MoreVertIcon fontSize="small" />
-      </div>
-      <div className="bottom">
-        <div className="featuredChart">
-          <CircularProgressbar value={50} text={"50%"} strokeWidth={5} />
+      <div className="left">
+        <div className="top">
+          <h1 className="title">Total Reviews</h1>
         </div>
-        <p className="title">Total Reviews Today</p>
-        <p className="amount">3</p>
+        <div className="bottom">
+          <div className="featuredChart">
+            <CircularProgressbar value={50} text={"50%"} strokeWidth={5} />
+          </div>
+          <p className="title">Total Reviews Today</p>
+          <p className="amount">3</p>
+          <div className="summary">
+            <div className="item">
+              <div className="itemTitle">Target</div>
+              <div className="itemResult negative">
+                <div className="resultAmount">$12.4k</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <div className="top">
+          <h1 className="title">Total Reservations</h1>
+        </div>
+        <div className="bottom">
+          <div className="featuredChart">
+            <CircularProgressbar value={50} text={"50%"} strokeWidth={5} />
+          </div>
+          <p className="title">Total Reservations Today</p>
+          <p className="amount">3</p>
+          <div className="summary">
+            <div className="item">
+              <div className="itemTitle">Target</div>
+              <div className="itemResult negative">
+                <div className="resultAmount">$12.4k</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
