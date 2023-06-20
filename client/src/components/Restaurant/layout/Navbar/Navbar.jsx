2743924bf842f3b4e4f06142d2ecd2f624d8b19d
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2023-06-20 18:38:22 +0600

    Sidebar and Navbar Updated

--- client/src/components/Restaurant/layout/Navbar/Navbar.jsx
+++ client/src/components/Restaurant/layout/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Navbar.scss";
 import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
+import FullscreenExitRoundedIcon from '@mui/icons-material/FullscreenExitRounded';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import DP from '../../../../img/M1syl.jpeg';
 
 const Navbar = () => {
   return (
@@ -8,8 +13,30 @@ const Navbar = () => {
         <div className="wrapper">
             <div className="items">
                 <div className="item">
+                    <LanguageRoundedIcon className='icon'/>
+                    English
+                </div>
+                <div className="item">
+                    <FullscreenExitRoundedIcon/>
+                    {/* Fullscreen */}
+                </div>
+                <div className="item">
+                    <DarkModeOutlinedIcon/>
+                    {/* Fullscreen */}
+                </div>
+                <div className="item">
                     <NotificationsNoneRoundedIcon/>
-                    Notifications
+                    {/* Notifications */}
+                </div>
+                <div className="item">
+                    <LogoutRoundedIcon/>
+                    {/* Fullscreen */}
+                </div>
+                <div className="item">
+                    <img src={DP} 
+                         alt="" 
+                         className='avatar'
+                    />
                 </div>
             </div>
         </div>
