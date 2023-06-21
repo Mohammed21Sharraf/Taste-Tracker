4ee315fa12949deac3163dc0d53adfb223c98830
KaziQuader <zunayedquader@gmail.com>
2023-06-21 14:16:26 +0600

    Fixed the customer review page

--- client/src/components/Restaurant/Customer/Review/Post.jsx
+++ client/src/components/Restaurant/Customer/Review/Post.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import './Post.scss'
+import React from "react";
+import "./Post.scss";
 
-
-const Post = ({data}) => {
+const Post = ({ data }) => {
   return (
     <div className="Post">
-        <img src={data.img} alt="" />
-        <div className="details">
-            <span>Name: {data.name} </span>
-        </div>
-        <span>Review: {data.review}</span>
+      <img className="img" src={data.img} alt="" />
+      <div className="details">
+        <span>Name: {data.name} </span>
+      </div>
+      <span>Review: {data.review}</span>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
--- client/src/components/Restaurant/Reservation/Datatable/Datatable.jsx
+++ client/src/components/Restaurant/Reservation/Datatable/Datatable.jsx
@@ -8,99 +8,97 @@ const Datatable = () => {
   let rows = [];
   let columns = [];
 
-  if (location.pathname === "/reservation") {
-    const options = [
-      {
-        userID: 1,
-        name: "Tahsin Ashrafee",
-        seatsBooked: 5,
-        totalRes: 2,
-        status: "Processing",
-      },
-      {
-        userID: 2,
-        name: "Tahsin Ashrafee",
-        seatsBooked: 5,
-        totalRes: 2,
-        status: "Approved",
-      },
-      {
-        userID: 3,
-        name: "Tahsin Ashrafee",
-        seatsBooked: 5,
-        totalRes: 2,
-        status: "Rejected",
-      },
-    ];
+  const options = [
+    {
+      userID: 1,
+      name: "Tahsin Ashrafee",
+      seatsBooked: 5,
+      totalRes: 2,
+      status: "Processing",
+    },
+    {
+      userID: 2,
+      name: "Tahsin Ashrafee",
+      seatsBooked: 5,
+      totalRes: 2,
+      status: "Approved",
+    },
+    {
+      userID: 3,
+      name: "Tahsin Ashrafee",
+      seatsBooked: 5,
+      totalRes: 2,
+      status: "Rejected",
+    },
+  ];
 
-    columns = [
-      { field: "id", headerName: "User ID", width: 70 },
+  columns = [
+    { field: "id", headerName: "User ID", width: 70 },
 
-      {
-        field: "name",
-        headerName: "User Name",
-        width: 250,
-      },
-      {
-        field: "seatsBooked",
-        headerName: "Seats Reserved",
-        type: "number",
-        width: 230,
-      },
+    {
+      field: "name",
+      headerName: "User Name",
+      width: 250,
+    },
+    {
+      field: "seatsBooked",
+      headerName: "Seats Reserved",
+      type: "number",
+      width: 230,
+    },
 
-      {
-        field: "totalRes",
-        headerName: "Total Reservations",
-        type: "number",
-        width: 230,
+    {
+      field: "totalRes",
+      headerName: "Total Reservations",
+      type: "number",
+      width: 230,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 230,
+      renderCell: (params) => {
+        return (
+          <div className={`cellWithStatus ${params.row.status}`}>
+            {params.row.status}
+          </div>
+        );
       },
-      {
-        field: "status",
-        headerName: "Status",
-        width: 230,
-        renderCell: (params) => {
-          return (
-            <div className={`cellWithStatus ${params.row.status}`}>
-              {params.row.status}
-            </div>
-          );
-        },
-      },
-      {
-        field: "action",
-        headerName: "Action",
-        width: 250,
-        renderCell: (params) => {
-          return (
-            <div>
-              {params.row.status === "Processing" ? (
-                <div className="cellAction">
-                  <div className="deleteButton">Reject</div>
-                  <div className="approveButton">Approve</div>
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <div>
+            {params.row.status === "Processing" ? (
+              <div className="cellAction">
+                <div className="deleteButton">Reject</div>
+                <div className="approveButton">Approve</div>
+              </div>
+            ) : (
+              <div className="cellAction">
+                <div className={`cellWithStatus ${params.row.status}`}>
+                  Processed
                 </div>
-              ) : (
-                <div className="cellAction">
-                  <div className={`cellWithStatus ${params.row.status}`}>
-                    Processed
-                  </div>
-                </div>
-              )}
-            </div>
-          );
-        },
+              </div>
+            )}
+          </div>
+        );
       },
-    ];
+    },
+  ];
 
-    options.forEach((item) => {
-      rows.push({
-        id: item.userID,
-        seatsBooked: item.seatsBooked,
-        totalRes: item.totalRes,
-        name: item.name,
-        status: item.status,
-      });
+  options.forEach((item) => {
+    rows.push({
+      id: item.userID,
+      seatsBooked: item.seatsBooked,
+      totalRes: item.totalRes,
+      name: item.name,
+      status: item.status,
     });
-  }
+  });
 
   return (
     <Fragment>
