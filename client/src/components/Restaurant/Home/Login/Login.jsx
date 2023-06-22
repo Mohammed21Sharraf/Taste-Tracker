168020fc2b4ccc15d30e4b2dc522bbd50d5e3d4e
Tahsin Ashrafee <tahsinashrafee99@gmail.com>
2023-06-23 04:43:42 +0600

    Added Multiple Users

--- client/src/components/Restaurant/Home/Login/Login.jsx
+++ client/src/components/Restaurant/Home/Login/Login.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
-import logo from "../../../../img/newlogo.png"
+import logo from "../../../../img/newlogotxt.png";
 
 const Login = () => {
+  const [userType, setUserType] = useState("restaurantOwner");
+
   const handleSignUpClick = () => {
     const container = document.querySelector(".container");
     container.classList.add("sign-up-mode");
@@ -13,11 +15,24 @@ const Login = () => {
     container.classList.remove("sign-up-mode");
   };
 
+  const handleUserTypeChange = (event) => {
+    setUserType(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (userType === "restaurantOwner") {
+      console.log("Restaurant Owner login or signup");
+    } else if (userType === "user") {
+      console.log("User login or signup");
+    }
+  };
+
   return (
     <div className="container">
       <div className="forms-container">
         <div className="signin-signup">
-          <form action="#" className="sign-in-form">
+          <form action="#" className="sign-in-form" onSubmit={handleSubmit}>
             <h2 className="title">Sign in</h2>
             <div className="input-field">
               <i className="fas fa-user"></i>
@@ -29,7 +44,7 @@ const Login = () => {
             </div>
             <input type="submit" value="Login" className="btn solid" />
           </form>
-          <form action="#" className="sign-up-form">
+          <form action="#" className="sign-up-form" onSubmit={handleSubmit}>
             <h2 className="title">Sign up</h2>
             <div className="input-field">
               <i className="fas fa-user"></i>
@@ -47,6 +62,12 @@ const Login = () => {
               <i className="fas fa-lock"></i>
               <input type="password" placeholder="Password" />
             </div>
+            <div className="user-type-field">
+              <select value={userType} onChange={handleUserTypeChange}>
+                <option value="restaurantOwner">Restaurant Owner</option>
+                <option value="user">User</option>
+              </select>
+            </div>
             <input type="submit" className="btn" value="Sign up" />
           </form>
         </div>
@@ -55,8 +76,8 @@ const Login = () => {
       <div className="panels-container">
         <div className="panel left-panel">
           <div className="content">
-            <h3>New here ?</h3>
-            <p>Sign Up Now !</p>
+            <h3>New here?</h3>
+            <p>Sign Up Now!</p>
             <button className="btn transparent" onClick={handleSignUpClick}>
               Sign up
             </button>
@@ -66,7 +87,7 @@ const Login = () => {
         <div className="panel right-panel">
           <div className="content">
             <h3>One of us?</h3>
-            <p>One click away from Signing in !</p>
+            <p>Sign In Now!</p>
             <button className="btn transparent" onClick={handleSignInClick}>
               Sign in
             </button>
