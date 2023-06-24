2d8ccc191f2fbfe563cb66c1d15037a6d1a4c7cc
KaziQuader <zunayedquader@gmail.com>
2023-06-24 17:56:26 +0600

    Comment added

--- server/controller/restaurantController.js
+++ server/controller/restaurantController.js
@@ -1,21 +1,21 @@
 import { Restaurant } from "../models/Restaurant";
 
+// Create a new Restaurant -- Restaurant Owner
 export const createRestaurant = async (req, res) => {
-    try {
-        const {resName, description, aov, logo, category, seats} = req.body;
+  try {
+    const { resName, description, aov, logo, category, seats } = req.body;
 
-        const newRes = await Restaurant.create({
-            resName,
-            description,
-            aov,
-            logo,
-            category,
-            seats,
-        });
+    const newRes = await Restaurant.create({
+      resName,
+      description,
+      aov,
+      logo,
+      category,
+      seats,
+    });
 
-        res.status(200).json("New Restaurant created!", newRes)
-    } catch (error) {
-        res.staus(500).json(error);
-    }
-    
-}
+    res.status(200).json("New Restaurant created!", newRes);
+  } catch (error) {
+    res.staus(500).json(error);
+  }
+};
