f3b0ddbfbe0c1707e25d3f321c0a7c49d16a8f0d
Tahsin Ashrafee <tahsinashrafee99@gmail.com>
2023-07-09 22:49:40 +0600

    Added navbar and header

--- client/src/components/EndUser/Homepage/Homepage.jsx
+++ client/src/components/EndUser/Homepage/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar';
+import Header from './Header/Header';
 
 const Homepage = () => {
   return (
     <div className="Homepage">
         <Navbar/>
+        <Header/>
+        <h1>Lorem ipsum</h1>
+        
     </div>
   )
 }
