5156c716178c0905b4c2a49c3f47e13582044fd0
Tahsin Ashrafee <tahsinashrafee99@gmail.com>
2023-07-10 02:51:35 +0600

    Added Restaurant Cards

--- client/src/components/EndUser/Homepage/Homepage.jsx
+++ client/src/components/EndUser/Homepage/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar';
 import Header from './Header/Header';
-
+import Cards from './RestaurantCard/Cards'
 const Homepage = () => {
   return (
     <div className="Homepage">
         <Navbar/>
         <Header/>
-        <h1>Lorem ipsum</h1>
+        <Cards/>
+
         
     </div>
   )
