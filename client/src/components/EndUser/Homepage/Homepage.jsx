749ff0a91447f168c1468a215bd085d03a9b9366
Tahsin Ashrafee <tahsinashrafee99@gmail.com>
2023-07-10 07:11:47 +0600

    RestaurantOwner Homepage View Added

--- client/src/components/EndUser/Homepage/Homepage.jsx
+++ client/src/components/EndUser/Homepage/Homepage.jsx
@@ -3,12 +3,15 @@ import Navbar from './Navbar/Navbar';
 import Header from './Header/Header';
 import Cards from './RestaurantCard/Cards'
 import Footer from './Footer/Footer'
+import RestaurantOwner from './RestaurantOwner/RestaurantOwner';
+
 const Homepage = () => {
   return (
     <div className="Homepage">
         <Navbar/>
         <Header/>
         <Cards/>
+        <RestaurantOwner/>
         <Footer/>
         
     </div>
