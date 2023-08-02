ca8fa9661ce5625e6029b6c2390bc847eb28415d
Tahsin Ashrafee <tahsinashrafee99@gmail.com>
2023-08-02 13:57:40 +0600

    Review Half Fixed

--- client/src/components/EndUser/Review/GetReviews/Reviews.jsx
+++ client/src/components/EndUser/Review/GetReviews/Reviews.jsx
@@ -8,8 +8,6 @@ import axios from "axios";
 import { baseURL } from "../../../../api";
 import { useParams } from "react-router-dom";
 
-// import { useParams } from 'react-router-dom';
-
 const Reviews = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [reviewData, setReviewData] = useState([]);
@@ -34,61 +32,40 @@ const Reviews = () => {
   }, [updateUI, id]);
 
   return (
-    <div className="Reviews">
-      <div className="mt-4 -mx-8 px-8 py-8">
-        <h1 className="text-5xl mx-3">{resData.name}</h1>
-        <div className="my-4 mx-5">
-          <h2 className="font-semibold text-2xl">Descriptions</h2>
+    <div className="Reviews-container">
+      <div className="Reviews-header">
+        <h1 className="Reviews-title">{resData.name}</h1>
+        <div className="Reviews-description">
+          <h2 className="Reviews-description-title">Descriptions</h2>
           <p>{resData.description}</p>
         </div>
-        <hr
-          style={{
-            background: "gray",
-            height: "0.5px",
-            margin: "50px",
-            border: "0.8px dotted",
-          }}
-        />
-        <div className="grid grid-cols-2">
-          <div className="mx-5">
-            <h4 className="font-semibold text-xl">
+        <hr className="Reviews-hr" />
+        <div className="Reviews-info">
+          <div className="Reviews-info-item">
+            <h4 className="Reviews-info-title">
               Average Order Value: {resData.averageOrderValue}
             </h4>
             <br />
-            <h4 className="text-xl font-semibold">
+            <h4 className="Reviews-info-title">
               Seat Capacity: {resData.capacity}
             </h4>
             <br />
           </div>
         </div>
-        <hr
-          style={{
-            background: "gray",
-            marginTop: "20px",
-            margin: "50px",
-            border: "0.8px dotted",
-          }}
-        />
-        <div className="my-4 mx-5">
-          <h2 className="font-semibold text-2xl">Category</h2>
+        <hr className="Reviews-hr" />
+        <div className="Reviews-category">
+          <h2 className="Reviews-category-title">Category</h2>
           {resData.category}
         </div>
-        <hr
-          style={{
-            background: "gray",
-            marginTop: "20px",
-            margin: "50px",
-            border: "0.8px dotted",
-          }}
-        />
-        <div className="my-4 mx-5">
-          <h2 className="font-semibold text-2xl">Reviews</h2>
+        <hr className="Reviews-hr" />
+        <div className="Reviews-reviews">
+          <h2 className="Reviews-reviews-title">Reviews</h2>
           <Grid
             container
             spacing={{ xs: 2, md: 3 }}
             columns={{ xs: 4, sm: 8, md: 12 }}
           >
-            {Array.from(Array(setReviewData.length)).map((_, index) => (
+            {Array.from(Array(reviewData.length)).map((_, index) => (
               <Grid xs={2} sm={4} md={4} key={index}>
                 {reviewData.map((review) => (
                   <ReviewCard
@@ -102,18 +79,15 @@ const Reviews = () => {
             ))}
           </Grid>
         </div>
-        <hr
-          style={{
-            background: "gray",
-            marginTop: "20px",
-            margin: "50px",
-            border: "0.8px dotted",
-          }}
-        />
-        <div className="bg-white shadow p-6 rounded 2xl w-1/4 mx-5 md:justify-center">
-          Share what you feel about the food!
-          <br />
-          <button className="submit-review" onClick={() => setModalOpen(true)}>
+        <hr className="Reviews-hr" />
+        <div className="Reviews-submit">
+          <div className="Reviews-submit-text">
+            Share what you feel about the food!
+          </div>
+          <button
+            className="Reviews-submit-button"
+            onClick={() => setModalOpen(true)}
+          >
             Submit Review
           </button>
         </div>
--- client/src/components/EndUser/Review/Review.jsx
+++ client/src/components/EndUser/Review/Review.jsx
@@ -1,19 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import UserNavbar from "../layout/UserNavbar/UserNavbar";
 import Reviews from "./GetReviews/Reviews";
 import Footer from "../layout/Footer/Footer";
 
 const Review = () => {
   return (
-    <div className="Review">
-      <UserNavbar />
-      <div>
-        <Reviews />
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
+    <Fragment>
+      <UserNavbar/>
+      <Reviews/>
+      <Footer/>
+    </Fragment>
   );
 };
 
