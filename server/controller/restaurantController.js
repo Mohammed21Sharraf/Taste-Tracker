dc73d628e254a4a2e5f1801b13e7ad314a3038b5
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2023-08-04 02:07:05 +0600

    Your Review Frontend and Backend Added

--- server/controller/restaurantController.js
+++ server/controller/restaurantController.js
@@ -130,7 +130,7 @@ export const createReview = async (req, res) => {
   const review = {
     user: req.user._id,
     name: req.user.name,
-    rating: ratings,
+    rating: Number(ratings),
     comment: comments,
     // img: images,
   };
@@ -147,6 +147,83 @@ export const createReview = async (req, res) => {
 };
 
 // Update Restaurant review
+
+export const updateReview = async (req, res) => {
+  const id = req.params.id;
+
+  const { rating, comment, reviewID } = req.body;
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment
+  };
+
+  const restaurant = await Restaurant.findById(id);
+  restaurant.reviews.forEach(rev => {
+    if (rev.user.toString() === req.user._id.toString()) {
+      if (rev._id.toString() === reviewID) {
+        (rev.rating = rating), (rev.comment = comment)
+      }
+    }
+  })
+
+  await restaurant.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    review
+  })
+}
+
+// Delete Single Restaurant Review 
+export const deleteReview = async (req, res) => {
+  const id = req.query.RestaurantID;
+
+  const { reviewID } = req.body;
+
+  const restaurant = await Restaurant.findById(id);
+
+  const reviews = restaurant.reviews.filter((rev) => rev._id.toString() !== reviewID)
+
+  // console.log(reviews);
+
+
+  await Restaurant.findByIdAndUpdate(
+    id,
+    {
+      reviews
+    }, {
+    new: true,
+    useFindAndModify: false
+  });
+
+  res.status(200).json({
+    success: true,
+
+  })
+}
+
+// Get reviews of a specific user 
+export const showMyReviews = async (req, res) => {
+  const id = req.params.id;
+
+  const restaurant = await Restaurant.findById(id);
+  let newReview = []
+
+  restaurant.reviews.forEach(rev => {
+    if (rev.user.toString() === req.user._id.toString()) {
+      console.log(rev);
+      newReview.push(rev)
+    }
+  })
+  res.status(200).json({
+    success: true,
+    newReview
+  })
+
+}
+
 
 // Get all restaurants
 export const getAllRestaurants = async (req, res) => {
